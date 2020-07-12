b7d3930708a31a8ffc812b92b1051e2bcbc8f14b
Tomás Pais <tomas.j.pais@gmail.com>
2020-07-12 19:24:31 -0300

    Changing annotation

--- src/test/kotlin/com/example/spring_api/unit/controllers/PingControllerTest.kt
+++ src/test/kotlin/com/example/spring_api/unit/controllers/PingControllerTest.kt
@@ -4,9 +4,10 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.example.spring_api.controllers.PingController
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
 class PingControllerTest {
     @Test
     fun handlePingTest() {
