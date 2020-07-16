365050b07aa437a579732d609e261fd3b1eb6c9a
Tomás Pais <tomas.j.pais@gmail.com>
2020-07-16 19:58:00 -0300

    Adding tests for a null param in genre

--- src/test/kotlin/com/example/spring_api/unit/controllers/UserControllerTest.kt
+++ src/test/kotlin/com/example/spring_api/unit/controllers/UserControllerTest.kt
@@ -20,6 +20,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -46,10 +47,15 @@ class UserControllerTest {
                 INACTIVE,
                 OTHER,
                 makeUsers(INACTIVE, OTHER)
+            ),
+            Arguments.of(
+                ACTIVE,
+                null,
+                makeUsers(INACTIVE)
             )
         )
 
-        private fun makeUsers(status: Status, genre: Genre, size: Int = 100): MutableList<User> {
+        private fun makeUsers(status: Status, genre: Genre = OTHER, size: Int = 100): MutableList<User> {
             val users = mutableListOf<User>()
 
             repeat(size) {
@@ -68,12 +74,29 @@ class UserControllerTest {
 
     @ParameterizedTest
     @MethodSource("filters")
-    fun getUsersByFiltersTest(status: Status, genre: Genre, expectedUsers: List<User>) {
+    fun getUsersByFiltersTest(status: Status, genre: Genre?, expectedUsers: List<User>) {
         // GIVEN
         every { userService.findAllByFilters(status, genre) } returns expectedUsers
 
         // WHEN
         val actualUsers = userController.getAllByFilters(status, genre)
+
+        // THEN
+        assertThat(actualUsers).all {
+            isNotEmpty()
+            hasSize(expectedUsers.size)
+            isEqualTo(expectedUsers)
+        }
+    }
+
+    @Test
+    fun getUsersWithoutGenreParamTest() {
+        // GIVEN
+        val expectedUsers = makeUsers(INACTIVE, size = 10)
+        every { userService.findAllByFilters(any(), any()) } returns expectedUsers
+
+        // WHEN
+        val actualUsers = userController.getAllByFilters(INACTIVE)
 
         // THEN
         assertThat(actualUsers).all {
