70913669c6e49476921e0472b578efed4e0b2bd4
Tomás Pais <tomas.j.pais@gmail.com>
2020-08-02 12:23:29 -0300

    Using environment variables to improve the use between local and production

--- src/main/kotlin/com/example/spring_api/databases/DatabaseInfo.kt
+++ src/main/kotlin/com/example/spring_api/databases/DatabaseInfo.kt
@@ -1,5 +1,6 @@
 package com.example.spring_api.databases
 
+import com.example.spring_api.error.exceptions.InvalidMySQLVersion
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.web.util.UriComponentsBuilder
@@ -20,21 +21,37 @@ open class DatabaseInfo(
         @NotBlank
         val dialect: String
     ) {
-        MYSQL(
+        MYSQL5(
             "com.mysql.jdbc.Driver",
             "org.hibernate.dialect.MySQL5Dialect"
-        ),
+        ) {
+            override fun toString() = super.toString().removeNumbers()
+        },
+        MYSQL8(
+            "com.mysql.cj.jdbc.Driver",
+            "org.hibernate.dialect.MySQL8Dialect"
+        ) {
+            override fun toString() = super.toString().removeNumbers()
+        },
         H2(
             "org.h2.Driver",
             "org.hibernate.dialect.H2Dialect"
         ) {
             override fun buildUrl(host: String, databaseName: String, extraParams: Map<String, String>?): String =
-                "jdbc:${name.toLowerCase()}:$host:$databaseName"
+                "jdbc:${toString()}:$host:$databaseName"
         };
+
+        companion object {
+            fun getMySQLType(mysqlVersion: Int) = when (mysqlVersion) {
+                5 -> MYSQL5
+                8 -> MYSQL8
+                else -> throw InvalidMySQLVersion(mysqlVersion)
+            }
+        }
 
         open fun buildUrl(host: String, databaseName: String, extraParams: Map<String, String>? = null): String {
             return UriComponentsBuilder.newInstance()
-                .scheme("jdbc:${name.toLowerCase()}")
+                .scheme("jdbc:${toString()}")
                 .host(host)
                 .path(databaseName)
                 .queryParams(
@@ -43,8 +60,12 @@ open class DatabaseInfo(
                 .build()
                 .toUriString()
         }
+
+        override fun toString() = name.toLowerCase()
     }
 }
+
+private fun String.removeNumbers() = this.replace(Regex("[0-9]"), "")
 
 private fun <K, V> Map<K, V>?.toMultiValueMap(): MultiValueMap<K, V>? {
     return this?.map {
