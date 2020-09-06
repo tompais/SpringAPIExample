64653a492b03618a59f6a36dfa38041cd6661a02
Tomás Pais <tomas.j.pais@gmail.com>
2020-09-06 17:43:56 -0300

    Adding unused import

--- src/test/kotlin/com/example/spring_api/unit/controllers/HelloControllerTest.kt
+++ src/test/kotlin/com/example/spring_api/unit/controllers/HelloControllerTest.kt
@@ -3,6 +3,7 @@ package com.example.spring_api.unit.controllers
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.example.spring_api.controllers.HelloController
+import com.example.spring_api.controllers.PingController
 import org.junit.jupiter.api.Test
 
 class HelloControllerTest {
